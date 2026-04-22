1728cb2fe77c95e132c983288f58e8fda4139154
siemteaghesogubay-git <siemteaghesogubay@gmail.com>
2026-04-22 10:33:34 +0200

    implemented css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
     <App />
